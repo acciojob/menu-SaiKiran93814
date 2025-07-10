3ef8d6c55f9cf1041f1b67a02302b9606d631447
SaiKiran93814 <teppali123@gmail.com>
2025-07-10 16:34:14 +0530

    changed the case

--- src/components/data.js
+++ src/components/data.js
@@ -2,7 +2,7 @@ const menu = [
   {
     id: 1,
     title: 'Buttermilk Pancakes',
-    category: 'breakfast',
+    category: 'Breakfast',
     price: '$15.99',
     img: 'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-1.jpeg',
     desc: 'I\'m baby woke mlkshk wolf bitters live-edge...',
@@ -10,7 +10,7 @@ const menu = [
   {
     id: 2,
     title: 'Diner Double',
-    category: 'lunch',
+    category: 'Lunch',
     price: '$13.99',
     img: 'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-2.jpeg',
     desc: 'Vaporware iPhone mumblecore selvage...',
@@ -18,7 +18,7 @@ const menu = [
   {
     id: 3,
     title: 'Godzilla Milkshake',
-    category: 'shakes',
+    category: 'Shakes',
     price: '$6.99',
     img: 'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-3.jpeg',
     desc: 'Ombucha chillwave fanny pack...',
@@ -26,7 +26,7 @@ const menu = [
   {
     id: 4,
     title: 'Country Delight',
-    category: 'breakfast',
+    category: 'Breakfast',
     price: '$20.99',
     img: 'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-4.jpeg',
     desc: 'Shabby chic keffiyeh neutra snackwave...',
@@ -34,7 +34,7 @@ const menu = [
   {
     id: 5,
     title: 'Egg Attack',
-    category: 'lunch',
+    category: 'Lunch',
     price: '$22.99',
     img: 'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-5.jpeg',
     desc: 'Franzen vegan pabst bicycle rights...',
@@ -42,7 +42,7 @@ const menu = [
   {
     id: 6,
     title: 'Oreo Dream',
-    category: 'shakes',
+    category: 'Shakes',
     price: '$18.99',
     img: 'https://vanilla-js-basic-project-8-menu.netlify.app/images/item-6.jpeg',
     desc: 'Portland chicharrones ethical...',
